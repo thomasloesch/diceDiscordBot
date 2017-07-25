79a04af89ba0a92f27e957a097e4d0efbd578cc0
thomasloesch <tommy.loesch@gmail.com>
2017-07-25 00:37:34 -0400

    Inital commit

---
+++ src/main/java/AnnotationListener.java
@@ -0,0 +1,9 @@
+import sx.blah.discord.api.events.EventSubscriber;
+import sx.blah.discord.handle.impl.events.ReadyEvent;
+
+public class AnnotationListener {
+    @EventSubscriber
+    public void onReadyEvent(ReadyEvent event) {
+
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,26 @@
+import sx.blah.discord.api.*;
+import sx.blah.discord.api.events.EventDispatcher;
+
+public class Main {
+    public static void main(String[] args) {
+        IDiscordClient client = createClient(args[0], true);
+        EventDispatcher dispatcher = client.getDispatcher();
+        //dispatcher.registerListener(new InterfaceListener());
+        dispatcher.registerListener(new AnnotationListener());
+    }
+
+    private static IDiscordClient createClient(String token, boolean login){
+        ClientBuilder clientBuilder = new ClientBuilder();
+        clientBuilder.withToken(token);
+        try {
+            if(login) {
+                return clientBuilder.login();
+            } else {
+                return clientBuilder.build();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
