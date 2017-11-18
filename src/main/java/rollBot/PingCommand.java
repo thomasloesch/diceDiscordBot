ea60015922189b5a1df5315285073972f0e071dc
thomasloesch <tommy.loesch@gmail.com>
2017-11-18 02:59:50 -0500

    Minor changes to encapsulate members.

--- src/main/java/rollBot/PingCommand.java
+++ src/main/java/rollBot/PingCommand.java
@@ -7,8 +7,8 @@ import org.slf4j.LoggerFactory;
 import java.util.function.Consumer;
 
 public class PingCommand implements Consumer<CommandContext> {
-    public static final String COMMAND_NAME = "ping";
-    Logger logger = LoggerFactory.getLogger(PingCommand.class);
+    private static final String COMMAND_NAME = "ping";
+    private Logger logger = LoggerFactory.getLogger(PingCommand.class);
 
     @Override
     public void accept(CommandContext ctx) {
