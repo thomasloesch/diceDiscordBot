edf9fe5c522d0268c168c32fb13af0741090f3b9
thomasloesch <tommy.loesch@gmail.com>
2017-11-18 02:59:13 -0500

    Added logging to ping.

--- src/main/java/rollBot/PingCommand.java
+++ src/main/java/rollBot/PingCommand.java
@@ -1,14 +1,18 @@
 package rollBot;
 
 import com.darichey.discord.CommandContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.function.Consumer;
 
 public class PingCommand implements Consumer<CommandContext> {
     public static final String COMMAND_NAME = "ping";
+    Logger logger = LoggerFactory.getLogger(PingCommand.class);
 
     @Override
     public void accept(CommandContext ctx) {
+        logger.info("Ping command received!");
         ctx.getChannel().sendMessage("Pong!");
     }
 }
